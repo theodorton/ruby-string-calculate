cd69dc8c4075dde66f89c583ab3b5f02bdad07f7
Theodor Tonum <theodor@tonum.no>
2011-09-12 12:23:58 +0200

    Joined some tests

--- calculate_test.rb
+++ calculate_test.rb
@@ -20,31 +20,16 @@ class CalculateTest < Test::Unit::TestCase
     assert_equal "60/5".calculate, 12
   end
   
-  def test_advanced_math_a
+  def test_advanced_math
     assert_equal "15+35-5*5".calculate, 25
-  end
-
-  def test_advanced_math_b
     assert_equal "5+10/2-8*4".calculate, -22
-  end
-
-  def test_advanced_math_c
     assert_equal "16*12/6+67-12+20/5".calculate, 91
   end
   
-  def test_floats_addition
+  def test_floats
     assert_equal "15.5 + 12.7".calculate, 28.2
-  end
-  
-  def test_floats_substraction
     assert_equal "7.24 - 4.12".calculate, 3.12
-  end
-  
-  def test_floats_multiplication
     assert_equal "4.5 * 3".calculate, 13.5
-  end
-  
-  def test_floats_division
     assert_equal "5.5 / 11".calculate, 0.5
   end
   
